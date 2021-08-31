eb7742d6fe5ed6bee7f31dcf97f02c6252f5c41d
JonHimself <jcbrstudio@gmail.com>
2021-08-31 14:55:00 -0500

    first rollbar

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const path = require('path')
+const Rollbar = require('rollbar')
 
+let rollbar = new Rollbar({
+    accessToken: '27a17fa91be242c585117698b52f0ebb',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 const app = express()
 
 app.use(express.json())
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
+    rollbar.info('html file served successfully.')
 })
 
 app.get('/index.js', (req,res) => {
