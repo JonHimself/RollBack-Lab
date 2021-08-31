50523930fd6048aa74aab70c05a441dbc63b2825
JonHimself <jcbrstudio@gmail.com>
2021-08-31 15:02:28 -0500

    rollbar test3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const Rollbar = require('rollbar')
 
 let rollbar = new Rollbar({
-    accessToken: 'f299ff01fdb54b3fab4a9c41ceeca549',
+    accessToken: '27a17fa91be242c585117698b52f0ebb',
     captureUncaught: true,
     captureUnhandledRejections: true
 })
@@ -18,8 +18,11 @@ app.get('/', (req,res) => {
 
 app.get('/index.js', (req,res) => {
     res.sendFile(path.join(__dirname, '/index.js'))
+    rollbar.info('html file served successfully.')
 })
 
 const port = process.env.PORT || 4545
+
+app.use(rollbar.errorHandler())
 
 app.listen(port, () => console.log(`Take us to warp ${port}!`)) 
