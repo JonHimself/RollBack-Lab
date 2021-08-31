e07648733140d815f15f6b304a92cb0dad87b074
JonHimself <jcbrstudio@gmail.com>
2021-08-31 16:59:20 -0500

    coins

--- server.js
+++ server.js
@@ -23,12 +23,13 @@ app.get('/', (req,res) => {
 app.post('/api/coinName', (req, res) => {
     let {coinName} = req.body
     rollbar.info('sent successfully')
+    coins.push(coinName)
+    
 for(let i = 0; i < coins.length; i++){
     if(coins[i] === coinName){
         rollbar.critical("Name exsists");
         res.status(400).send('you have entered this already')
         }else if (coins[i] !== coinName){
-            coins.push(coinName)
             rollbar.warning("added");
             res.status(400).send('careful, do not double enter')
         }  
